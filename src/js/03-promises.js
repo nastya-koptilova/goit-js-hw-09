d69dbed6b17bfa71f512078e0034be775e02be3a
nastya-koptilova <koptilova.nastya@gmail.com>
2023-01-11 01:22:38 +0200

    add task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit(event) {
+  event.preventDefault();
+  const { amount, delay, step } = event.target.elements;
+  let delayNumber = Number(delay.value);
+  let stepNumber = Number(step.value);
+  let amountNumber = Number(amount.value);
+  for (let i = 0; i < amountNumber; i += 1) {
+    createPromise(i + 1, delayNumber)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayNumber += stepNumber;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
