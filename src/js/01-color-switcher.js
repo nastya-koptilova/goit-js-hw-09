b418bcfa8674ee6f0f0e5a5035b8b7cf4c46bd7b
nastya-koptilova <koptilova.nastya@gmail.com>
2023-01-10 21:33:48 +0200

    add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+stopButton.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onBodyChangeColor = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+};
+
+const onBodyStopChangeColor = () => {
+  clearInterval(timerId);
+  stopButton.disabled = true;
+  startButton.disabled = false;
+};
+
+startButton.addEventListener('click', onBodyChangeColor);
+stopButton.addEventListener('click', onBodyStopChangeColor);
