df8b6ad50550b3871e6980adc05218d44eca4565
nastya-koptilova <koptilova.nastya@gmail.com>
2023-01-11 02:19:04 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,8 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-stopButton.disabled = true;
 let timerId = null;
+stopButton.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
